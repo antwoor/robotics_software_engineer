6302f4b97650241bd96452210b6af092b01279e0
antwoor <geroev_sasha@mail.ru>
2024-10-06 16:47:26 +0300

    launch.py works properelly

---
+++ module_2_assignment/launch/task_2.launch.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    TurtleSim = Node(
+        package='turtlesim',
+        executable='turtlesim_node',
+        name='simulator'
+    )
+    radius = '1'  #initial radius of spiral
+    growth_rate = '0.1'  # koefficient of spiral growth
+    SpiralTurtle = Node(
+        package='module_2_assignment',
+        executable='task1_spiral',
+        name='archi_spiral',
+        arguments=[radius, growth_rate]    
+    )
+    return LaunchDescription([
+        TurtleSim, SpiralTurtle
+    ])
--- module_2_assignment/src/task_1/turtle_spiral.h
+++ module_2_assignment/src/task_1/turtle_spiral.h
@@ -4,7 +4,6 @@ class SpiralPublisher : public TrajectoryPublisher {
 public:
     SpiralPublisher(const int& a, const int& k);
     double r_, phi_, a_, k_;
-    size_t time_;
 protected:
     void timer_callback() override; 
 };
